2ee1228ab90d01834a42f2f2890c24571935472b
peter hosking <p.a.g.hosking@gmail.com>
2014-06-07 14:23:51 +1200

    auto polyline replacement wells detected and filled in

--- dxf_checker.py
+++ dxf_checker.py
@@ -1,23 +1,42 @@
 import ezdxf
+import math
 
-dwg = ezdxf.readfile("hello.dxf")
+file_name = "square.dxf"
+
+dwg = ezdxf.readfile(file_name)
 modelspace = dwg.modelspace()
-illegal_entity_count = 0
 
-
+# draw LINES instead of POLYLINES
 for e in modelspace:
-    print("DXF Entity: %s\n" % e.dxftype())
-    if not (e.dxftype() == "LINE" or e.dxftype() == "ARC" or e.dxftype() == "VIEWPORT"):
+    if e.dxftype() == "POLYLINE":
+        point_array = []
+        for p in e.points():
+            point_array.append(p)
+        for i in range(0, len(point_array)-1):
+            modelspace.add_line(point_array[i], point_array[i+1])
+        if e.dxf.flags == 1:  # check if the polyline is closed or not
+            modelspace.add_line(point_array[len(point_array)-1], point_array[0])
+
+# delete POLYLINES
+polylines_remaining = 0
+for e in modelspace:
+    if e.dxftype() == "POLYLINE":
+        polylines_remaining = 1
+while polylines_remaining:
+    for e in modelspace:
         if e.dxftype() == "POLYLINE":
-            # print ("Polyline type: %s" % e.get_mode())
-            point_array = []
-            for p in e.points():
-                point_array.append(p)
-            # print point_array
             modelspace.delete_entity(e)
-            for i in range(0, len(point_array)-1):
-                modelspace.add_line(point_array[i], point_array[i+1])
-        print ("illegal entity: %s" % e.dxftype())
+    polylines_remaining = 0
+    for e in modelspace:
+        if e.dxftype() == "POLYLINE":
+            polylines_remaining = 1
+
+# check file for illegal entities
+illegal_entity_count = 0
+for e in modelspace:
+    # print("DXF Entity: %s\n" % e.dxftype())
+    if not (e.dxftype() == "LINE" or e.dxftype() == "ARC" or e.dxftype() == "VIEWPORT" or e.dxftype() == "CIRCLE"):
+        print "illegal entity: " + e.dxftype()
         illegal_entity_count += 1
 
 if not illegal_entity_count:
@@ -25,4 +44,19 @@ if not illegal_entity_count:
 else:
     print "%s illegal entities detected" % illegal_entity_count
 
-dwg.saveas("hello_modified_2.dxf")
+# check design is within size limit
+pass
+
+# locate and draw wells
+for e in modelspace:
+    if e.dxftype() == "CIRCLE":
+        radius = e.get_dxf_attrib("radius")
+        color = e.get_dxf_attrib("color")
+        center = e.get_dxf_attrib("center")
+        if color == 240 and radius < 6:
+            for i in range(0, int(math.floor(radius/0.1))-1):
+                modelspace.add_circle(center, radius-0.1*(i+1))
+
+
+
+dwg.saveas(file_name.replace('.dxf', '') + "_converted.dxf")
